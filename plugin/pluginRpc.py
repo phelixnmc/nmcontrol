b3a33c6b1f4eb72e16662404b945be7710e27c0d
phelixnmc <githubnmc@blockchained.com>
2014-09-01 18:54:44 +0200

    Make Nmcontrol stopable via ctrl-c on windows

--- plugin/pluginRpc.py
+++ plugin/pluginRpc.py
@@ -19,16 +19,19 @@ class pluginRpc(plugin.PluginThread):
         self.threads = []
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.settimeout(0.5)
         try:
             self.s.bind((self.conf['host'], int(self.conf['port'])))
             self.s.listen(1)
             while self.running:
                 try:
-                    c = rpcClientThread(self.s.accept(), app)
+                    acc = self.s.accept()
+                    c = rpcClientThread(acc, app)
                     c.start()
                     self.threads.append(c)
                 except Exception as e:
-                    if app['debug']: print "except:", e
+                    if app['debug'] and str(e) != "timed out":
+                        print "except:", e
         except KeyboardInterrupt:
             pass
         except:
