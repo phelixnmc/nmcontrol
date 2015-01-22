93f24397f26a1613981251aa9df7ac840920f865
phelixnmc <githubnmc@blockchained.com>
2015-01-22 14:38:30 +0100

    Add app icon

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
 C:\Python27\Scripts\pyinstaller -p lib;plugin;service ^
     --hidden-import=asyncore --hidden-import=win32gui_struct --hidden-import=win32gui ^
     --hidden-import=winxpgui --hidden-import=commctrl --hidden-import=pywintypes ^
-    --noconfirm --noconsole nmcontrolwin.pyw
+    --noconfirm --noconsole --icon=./lib/icon.ico nmcontrolwin.pyw
 
 @if %errorlevel% EQU 0 goto continue
 @pause
