14c545ecac90f79c76bae2d9f2292601d3b4cf5a
phelixnmc <githubnmc@blockchained.com>
2015-07-24 18:27:21 +0200

    Revert "Compile with Pyinstaller on Windows"
    This reverts commit ba888c4bed36d9136a7d826033f552b855b8700f.
    
    Conflicts:
    	build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,22 +1,9 @@
-C:\Python27\Scripts\pyinstaller -p lib;plugin;service ^
-    --hidden-import=asyncore --hidden-import=win32gui_struct --hidden-import=win32gui ^
-    --hidden-import=winxpgui --hidden-import=commctrl --hidden-import=pywintypes ^
-    --noconfirm --noconsole --icon=./lib/icon.ico nmcontrolwin.pyw
+C:\Python27\Scripts\pyinstaller -p lib;plugin;service --hidden-import=asyncore nmcontrol.py
 
-@if %errorlevel% EQU 0 goto continue
-@pause
-:continue
+mkdir dist\nmcontrol\lib\
+mkdir dist\nmcontrol\plugin\
+mkdir dist\nmcontrol\service\
 
-mkdir dist\nmcontrolwin\lib\
-mkdir dist\nmcontrolwin\plugin\
-mkdir dist\nmcontrolwin\service\
-
-xcopy lib dist\nmcontrolwin\lib /s /e /h /y
-xcopy plugin dist\nmcontrolwin\plugin /s /e /h /y
-xcopy service dist\nmcontrolwin\service /s /e /h /y
-
-rmdir dist\nmcontrolwin\_MEI\tk\images /s /q
-rmdir dist\nmcontrolwin\_MEI\tcl\tzdata /s /q
-rmdir dist\nmcontrolwin\_MEI\tcl\msgs /s /q
-
-del dist\*.pyc /s /q
+xcopy lib dist\nmcontrol\lib /s /e /h
+xcopy plugin dist\nmcontrol\plugin /s /e /h
+xcopy service dist\nmcontrol\service /s /e /h
